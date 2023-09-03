23e118319f61788a88e896e69403bef4aea450d7
GospoDeveloper <76654629+GospoDeveloper@users.noreply.github.com>
2023-09-03 12:02:07 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ let loginWindow = document.getElementById("loginWindow");
 let navLogin = document.getElementById("navLogin");
 let continut = document.getElementById("continut");
 
+// display la click
 let num = 0;
 navLogin.addEventListener("click", function () {
   num++;
@@ -32,4 +33,39 @@ navLogin.addEventListener("click", function () {
     loginWindow.classList.remove("displayNone");
     continut.classList.remove("displayOff");
   }
+});
+
+let formLogin = document.getElementById("formLogin");
+let inputUtilizator = document.querySelector(".inputUtilizator");
+let inputParola = document.querySelector(".inputParola");
+let buttonLogin = document.getElementById("btnLogin");
+let buttonForgot = document.getElementById("btnForgot");
+let formRegister = document.getElementById("formRegister");
+let inputCreeareEmail = document.querySelector(".inputCreeareEmail");
+let inputCreeareParola = document.querySelector(".inputCreeareParola");
+let inputReintroducereParola = document.querySelector(
+  ".inputReintroducereParola"
+);
+let buttonRegister = document.getElementById("btnRegister");
+
+//trimitere date user
+buttonRegister.addEventListener("click", function (e) {
+  e.preventDefault();
+  const user = inputCreeareEmail.value.toString().trim().toLowerCase();
+  console.log(user);
+  const parola = inputCreeareParola.value.toString().trim();
+  console.log(parola);
+  const repetareParola = inputReintroducereParola.value.toString().trim();
+  console.log(repetareParola);
+  alert("Functie inca nedefinita. Disponibila in curand");
+});
+
+buttonLogin.addEventListener("click", function (e) {
+  e.preventDefault();
+  alert("Functie inca nedefinita. Disponibila in curand");
+});
+
+buttonForgot.addEventListener("click", function (e) {
+  e.preventDefault();
+  alert("Functie inca nedefinita. Disponibila in curand");
 });
